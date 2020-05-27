4dc6beab09b794b3927fbe231bf23ae2f3fa433c
Nya Garcia <nyablk97@gmail.com>
2020-05-27 19:54:45 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { C4SComponent } from "./pages/c4s/c4s.component";
+import { HomeComponent } from "./pages/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "call-for-speakers", component: C4SComponent },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
