9995c7764c1e83814761d09a8d6871b839bef313
Nya Garcia <nyablk97@gmail.com>
2020-05-27 19:54:26 +0200

    feat(pages.module): add C4SComponent and MarkdownModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { HomeComponent } from "./home/home.component";
-import { ComponentsModule } from "../components/components.module";
+import { C4SComponent } from "./c4s/c4s.component";
+import { MarkdownModule } from "ngx-markdown";
+import { HomeModule } from "./home/home.module";
+
+const COMPONENTS = [C4SComponent];
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, ComponentsModule],
-  exports: [HomeComponent]
+  declarations: [...COMPONENTS],
+  imports: [CommonModule, MarkdownModule, HomeModule],
+  exports: [...COMPONENTS]
 })
 export class PagesModule {}
