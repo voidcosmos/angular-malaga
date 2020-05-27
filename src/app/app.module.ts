a6c091d658c161769c2c28e13998bc64b885e8bb
Nya Garcia <nyablk97@gmail.com>
2020-05-27 19:56:05 +0200

    feat(app.module): add FontAwesome, Layout and Markdown modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { PagesModule } from "./pages/pages.module";
-import { MaterialModule } from "./material/material.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MarkdownModule } from "ngx-markdown";
+import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterialModule,
     BrowserAnimationsModule,
-    PagesModule
+    PagesModule,
+    LayoutModule,
+    FontAwesomeModule,
+    MarkdownModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
