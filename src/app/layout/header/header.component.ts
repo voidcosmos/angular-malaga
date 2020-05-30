3eebcfad056610c0a582b783e280ff807ad7bda2
Nya Garcia <nyablk97@gmail.com>
2020-05-30 11:29:19 +0200

    feat(header.component.ts): add scroll listener

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import {
   faTwitter,
   faYoutube,
@@ -11,7 +11,19 @@ import {
   styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent {
+  showHeader: boolean = false;
   faTwitter = faTwitter;
   faYoutube = faYoutube;
   faMeetup = faMeetup;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    scrollOffset > 300 ? (this.showHeader = true) : (this.showHeader = false);
+  }
 }
