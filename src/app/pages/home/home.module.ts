ff985671bc9e9a2aac667445b4eef05b4aa1aeef
Nya Garcia <nyablk97@gmail.com>
2020-05-30 11:32:35 +0200

    feat(home.module): import modules

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./home.component";
 import { MainImageComponent } from "./components/main-image/main-image.component";
+import { SharedModule } from "@shared/shared.module";
+import { BannerComponent } from "./components/banner/banner.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-const COMPONENTS = [HomeComponent, MainImageComponent];
+const COMPONENTS = [HomeComponent, MainImageComponent, BannerComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, FlexLayoutModule],
   exports: [...COMPONENTS]
 })
 export class HomeModule {}
