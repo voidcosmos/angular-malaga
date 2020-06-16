8ef24ba6a0f0cb75ae7d53ebbd1916772154566c
Nya Garcia <nyablk97@gmail.com>
2020-06-16 17:36:51 +0200

    feat(home.module): import GalleryPreviewcomponent

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,8 +5,14 @@ import { MainImageComponent } from "./components/main-image/main-image.component
 import { SharedModule } from "@shared/shared.module";
 import { BannerComponent } from "./components/banner/banner.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { GalleryPreviewComponent } from "./components/gallery-preview/gallery-preview.component";
 
-const COMPONENTS = [HomeComponent, MainImageComponent, BannerComponent];
+const COMPONENTS = [
+  HomeComponent,
+  MainImageComponent,
+  BannerComponent,
+  GalleryPreviewComponent
+];
 
 @NgModule({
   declarations: [...COMPONENTS],
