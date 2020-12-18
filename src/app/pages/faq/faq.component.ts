4555e1fa5b3362f4163e445e9708f03f229db71d
Nya Garcia <nyablk97@gmail.com>
2020-12-18 19:11:19 +0100

    feat(pages): add faq

---
+++ src/app/pages/faq/faq.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-faq",
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: "./faq.component.html",
+  styleUrls: ["./faq.component.css"],
+})
+export class FaqComponent {}
