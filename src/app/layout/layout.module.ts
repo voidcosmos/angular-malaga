d40a363f167932ae1ba3f52513b339b20ef32f2d
Nya Garcia <nyablk97@gmail.com>
2020-12-18 19:21:02 +0100

    feat(layout.module): update module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,21 +3,27 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { RouterModule } from "@angular/router";
+import { SidenavComponent } from "./sidenav/sidenav.component";
 
+const COMPONENTS = [HeaderComponent, FooterComponent, SidenavComponent];
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [...COMPONENTS],
   imports: [
     CommonModule,
-    MatToolbarModule,
-    MatCardModule,
     FlexLayoutModule,
     FontAwesomeModule,
-    RouterModule
+    MatCardModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule,
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [...COMPONENTS],
 })
 export class LayoutModule {}
