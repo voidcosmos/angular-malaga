26137a7dc40bb01b8c1027916fee747f62e319bc
Nya Garcia <nyablk97@gmail.com>
2020-12-18 19:16:33 +0100

    feat(shared.module): update module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { RouterModule } from "@angular/router";
-import { TeamComponent } from "./components/team/team.component";
 import { MatCardModule } from "@angular/material/card";
+import { GalleryComponent } from "./components/mosaic-gallery/mosaic-gallery.component";
+import { TeamListComponent } from "./components/team-list/team-list.component";
 
+const COMPONENTS = [TeamListComponent, GalleryComponent];
 @NgModule({
-  declarations: [TeamComponent],
+  declarations: [...COMPONENTS, TeamListComponent],
   imports: [CommonModule, FlexLayoutModule, RouterModule, MatCardModule],
-  exports: [TeamComponent]
+  exports: [...COMPONENTS],
 })
 export class SharedModule {}
