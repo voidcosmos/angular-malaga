5925eac1e17634f6710579f7a099d8fc38c37485
Nya Garcia <nyablk97@gmail.com>
2020-12-18 19:17:59 +0100

    refactor(app-routing.module): lazy load pages module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { C4SComponent } from "./pages/c4s/c4s.component";
-import { HomeComponent } from "./pages/home/home.component";
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "call-for-speakers", component: C4SComponent },
+  {
+    path: "",
+    loadChildren: () =>
+      import("./pages/pages.module").then((m) => m.PagesModule),
+  },
   {
     path: "**",
-    redirectTo: ""
-  }
+    redirectTo: "",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
