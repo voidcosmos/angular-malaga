c751b58c4538fd0a32e22f409f5ce0aaaad7e55d
Nya Garcia <nyablk97@gmail.com>
2020-12-21 18:06:43 +0100

    feat(team-members.ts): update bios

--- src/app/shared/constants/team-members.ts
+++ src/app/shared/constants/team-members.ts
@@ -5,7 +5,8 @@ export const TEAM_MEMBERS: Organizer[] = [
     id: 1,
     name: "Estefanía García Gallardo",
     photo: "nya.jpeg",
-    bio: "",
+    bio:
+      "Soy desarrolladora full stack y amante del open source <3. Entre los proyectos para la comunidad que he creado están <a href='https://npkill.js.org/'>Npkill</a> y la documentación española de RxJS. A veces escribo artículos técnicos y soy editora de <a href='https://medium.com/puntotech'>PuntoTech</a>. También doy charlas de programación por el mundo.",
     socialMedia: [
       { platform: "twitter", url: "https://twitter.com/nyablk" },
       { platform: "github", url: "https://github.com/nyagarcia" },
@@ -16,7 +17,7 @@ export const TEAM_MEMBERS: Organizer[] = [
     name: "Carlos Caballero González",
     photo: "carlos.jpg",
     bio:
-      "Hi! My name is Carlos Caballero and I'm a PhD. in Computer Science from Málaga, Spain. Teaching developers and degree/master computer science how to be experts in web technologies and computer science fundamentals is what I do. I'm teaching in a vocational center and the University Oberta of Cataluya. <br> Also, I teach the companies which ask me specific courses in Angular, TypeScript, NodeJS or any topic about development. I have collaborated with universities in research tasks, as a result of which I have given several talks around of the world and obtained a PhD. in computer science. In addition, I have written many books about computer science for computer students from networks to Web programming.",
+      "Carlos Caballero González es Ingeniero en Informática y Doctor en Informática por la Universidad de Málaga. Máster en Ingeniería del Software e Inteligencia Artificial. Es profesor en la Universidad Oberta de Cataluña y la Junta de Andalucía en la familia profesional de Informática y Telecomunicaciones. Ha escrito varios libros docentes y más de una decena de artículos de investigación en las primeras revistas a nivel internacional como son las publicaciones en Solar Physics (A Comparative Study of Clustering Methods for Active Region Detection in Solar EUV Images; Automatic tracking of active regions and detection of solar flares on solar EUV images).",
     socialMedia: [
       { platform: "twitter", url: "https://twitter.com/carlillo" },
       { platform: "github", url: "https://github.com/caballerog" },
@@ -28,7 +29,7 @@ export const TEAM_MEMBERS: Organizer[] = [
     name: "Juan Torres Gómez",
     photo: "juan.png",
     bio:
-      "I love open source, the WEB, pizza and resolving problems :D Making stuff, all the time. Creator of the Npkill project. <br> <ul><li> Full Stack </li> <li> Sysadmin </li><li> Videogames DEV </li><li> Maker </li> </ul> <br> Oh, and I also love to contribute to open source!",
+      "Me encanta la programación, la WEB, la pizza y resolver problemas :D Siempre creando. Creador del proyecto <a href='https://npkill.js.org/'>Npkill</a>. <br> <ul><li> Full Stack </li> <li> Sysadmin </li><li> Desarrollador de Videojuegos </li><li> Creador </li> </ul> <br> Ah, ¡También me encanta contribuir al open source!",
     socialMedia: [
       { platform: "twitter", url: "https://twitter.com/juaniman99" },
       { platform: "github", url: "https://github.com/zaldih" },
