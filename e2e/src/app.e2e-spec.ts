8c671d17d94c4a9baafdb5b864668ff425344508
NyaGarcia <37065748+NyaGarcia@users.noreply.github.com>
2021-04-23 16:01:47 +0000

    Deploying to gh-pages from  @ 73407fab5f9462a8fde5c79aae3b3d2f0ba828a5 🚀

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,26 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging } from "protractor";
 
-describe('workspace-project App', () => {
+import { AppPage } from "./app.po";
+
+describe("workspace-project App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-malaga-website app is running!');
+    expect(page.getTitleText()).toEqual("angular-malaga app is running!");
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
